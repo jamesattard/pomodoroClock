c955179c4d95e8e2b7ab2ec13110ebda8cce1aa8
James Attard <attaj002@mfsa.internal>
2017-02-15 16:15:47 +0100

    fixed elapsed time marker

--- js/index.js
+++ js/index.js
@@ -1,35 +1,43 @@
 $(document).ready(function(){
 
   // Timer constructor
-  function Timer(duration) {
-    var timer;
-    var globalInterval;
-    this.masterInterval = duration;
+  function Timer(interval) {
+    this.interval = interval;
+    this.e = {};
   }
 
-  Timer.prototype.startTimer = function() {
-    var interval = this.masterInterval;
-    timer = setInterval(function(){
-      if(interval > 0){
+  Timer.prototype.startTimer = function(timerId) {
+    var interval = this.interval;
+    var e = this.e;
+
+    this.timer = setInterval(function(){
+      if (interval > 0){
+        elapsed = interval;
+        el = e;
+        el[timerId] = interval;
+        console.log(e);
         interval--;
-        globalInterval = interval;
-        console.log(interval);
       }
       else {
-        clearInterval(timer);
+        clearInterval(this.timer);
       }
     },1000);
   }
 
-  Timer.prototype.stopTimer = function() {
-    console.log("Finished at: ", globalInterval);
-    clearInterval(timer);
+  Timer.prototype.stopTimer = function(timerId) {
+    console.log("finished at: ", el);
+    clearInterval(this.timer);
   };
 
+  // Instantiate timers
   var myTimer = new Timer(10);
   var timer2 = new Timer(30);
-  timer2.startTimer();
-  $('#startBtn').click(function(){myTimer.startTimer();})
-  $('#stopBtn').click(function(){myTimer.stopTimer();})
+
+  // Start auto timer
+  //timer2.startTimer(2);
+
+  // Bind another timer with click event
+  $('#startBtn').click(function(){myTimer.startTimer(1);})
+  $('#stopBtn').click(function(){myTimer.stopTimer(1);})
 
 }); // end of DRF
