49a8b2d95536fd10dbe5d730b04f59f32fa37eee
James Attard <attaj002@mfsa.internal>
2017-02-20 17:42:10 +0100

    created button states

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   // Timer constructor
   function Timer(interval) {    // interval in mins
     var self = this;            // Save object scope
-    var minutes, seconds;
     var rstInterval = interval; // Save interval value
     var intervalSec = interval * 60;   // Convert to seconds
+    var minutes, seconds;
 
     self.startTimer = function() {
       self.handler = setInterval(function() {
@@ -41,8 +41,11 @@ $(document).ready(function() {
     };
 
     self.displayTimer = function(reset) {
+      console.log("reset: ", reset);
+      console.log("seconds: ", seconds);
+      console.log("minutes: ", minutes);
       if (reset === 0) {         // If reset === 0, resume timerDisplay
-        if (seconds < 10) {
+        if (seconds < 10 && seconds.length == 1) {
           seconds = "0" + seconds;
         }
         if (minutes < 10) {
@@ -71,8 +74,37 @@ $(document).ready(function() {
   var pomodoro = new Timer(sessionLen);
   var breaker = new Timer(breakLen);
 
-  // Bind timers with click event
-  $('#startBtn').click(function(){pomodoro.startTimer();})
-  $('#resetBtn').click(function(){pomodoro.resetTimer();})
+  // Handle Start click events
+  $('#startBtn').click(function() {
+    // console.log($("#startBtn > i").css("fa-play"));
+    if (document.getElementById("startBtn").hasAttribute("enabled")) {
+      // It's working time!
+      $("#status").text("working");
+      $("#status").css("color", "#FF0");
+      // Set button states
+      $("#startBtn > i").attr("class", "fa fa-pause");
+      $("#resetBtn").removeAttr("disabled");
+      $("#resetBtn").attr("enabled"); // could be superflous
+      $("#startBtn").removeAttr("enabled");
+      $("#resetBtn").attr("disabled"); // could be superflous
+      // Start the Pomodoro Timer
+      pomodoro.startTimer();
+    } else {
+      $("#status").text("--");
+      // Pause timer
+      pomodoro.pauseTimer();
+      // Set button states
+      $("#startBtn > i").attr("class", "fa fa-play");
+      $("#resetBtn").removeAttr("disabled");
+      $("#resetBtn").attr("enabled"); // could be superflous
+      $("#startBtn").removeAttr("enabled");
+      $("#resetBtn").attr("disabled"); // could be superflous
+    }
+  });
+
+  // Handle Reset click events
+  $('#resetBtn').click(function() {
+    pomodoro.resetTimer();
+  });
 
 }); // end of DRF
